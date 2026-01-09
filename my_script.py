2b3309b68dc042ac8548f554f2bbb8fdbb3e9e3e
dalamgir <daiyan.alamgir@gmail.com>
2026-01-09 10:51:08 -0500

    i modified my script

--- my_script.py
+++ my_script.py
@@ -1,6 +1,1 @@
-x = "This is a change I made from databricks"
-print(x)
-print("I made a nasty change!!!!")
-print("I made a nasty change!!!!")
-print("I made a nasty change!!!!")
-print("I made a nasty change!!!!")
+print("hello from smus")
